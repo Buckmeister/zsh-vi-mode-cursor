5da8d06ad770e5bf34cdf49419e15cae64a24d3a
Thomas Burk <Buckmeister@shellraiser.com>
2020-06-04 21:26:40 +0200

    Undo aliasing

--- zsh-vim-mode-cursor.zsh
+++ zsh-vim-mode-cursor.zsh
@@ -1,5 +1,4 @@
 # Change cursor according to vim-mode
-zle -A zle-keymap-select _vmc_zle-keymap-select
 function _vmc_zle-keymap-select {
   if [[ ${KEYMAP} == vicmd ]]; then
      echo -ne '\e[1 q'
@@ -11,13 +10,12 @@ function _vmc_zle-keymap-select {
 
   zle reset-prompt
 }
-zle -N _vmc_zle-keymap-select
+zle -N zle-keymap-select _vmc_zle-keymap-select
 
-zle -A zle-line-finish _vmc_zle-line-finish
 function _vmc_zle-line-finish {
   echo -ne '\e[5 q'
 }
-zle -N _vmc_zle-line-finish
+zle -N zle-line-finish _vmc_zle-line-finish
 
 _fix_cursor() {
    echo -ne '\e[5 q'
